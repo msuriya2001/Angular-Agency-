d9934b2781d4b8531f42cd5249882f6e784e8d68
msuriya2001 <suriyacse2001@gmail.com>
2024-03-02 22:18:10 +0530

    Added new dynamic form array only add table function added

--- src/app/adding-products/adding-products.component.ts
+++ src/app/adding-products/adding-products.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms'; 
+import { FormGroup } from '@angular/forms';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-adding-products',
   templateUrl: './adding-products.component.html',
   styleUrl: './adding-products.component.css'
 })
-export class AddingProductsComponent {
+export class AddingProductsComponent implements OnInit{
+
+  addProductsForm: FormGroup;
+
+  ngOnInit(): void {
+    
+    this.addProductsForm = new FormGroup({
+
+    productsArray:new FormArray([
+
+       // this is the form for products 
+
+      new FormGroup({
+
+        Name:new FormControl(null), 
+        Price:new FormControl(null),
+        Category:new FormControl(null), 
+        Available:new FormControl(null)
+
+      })
+
+      
+    ])
+
+
+    })
+
+
+  }
+
+  
+
+  AddNewProduct(){
+
+    const newProduct = new FormGroup({
+
+      Name:new FormControl(null), 
+      Price:new FormControl(null),
+      Category:new FormControl(null), 
+      Available:new FormControl(null)
+
+    });
+
+
+
+    (<FormArray>this.addProductsForm.get('productsArray')).push(newProduct);
+
+
+
+
+
+  }
+
+  SubmitForm(){
+
+
+    
+  }
+
 
 }
