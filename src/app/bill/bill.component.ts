40eb1e1d7a7bfc1a2ee9f92b0cfce551916eeb78
msuriya2001 <suriyacse2001@gmail.com>
2024-03-05 01:32:11 +0530

    bill componennt is added

---
+++ src/app/bill/bill.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bill',
+  templateUrl: './bill.component.html',
+  styleUrl: './bill.component.css'
+})
+export class BillComponent {
+
+}
